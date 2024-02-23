d75123e55aa9baca8dc9ee67e40e4876fc1300b0
Madhusudan <msberad99@gmail.com>
2024-02-23 13:04:22 +0530

    first commit

---
+++ demo/src/main/java/com/example/demo/service/PersonService.java
@@ -0,0 +1,43 @@
+package com.example.demo.service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.PersonDao;
+import com.example.demo.model.Person;
+
+@Service
+public class PersonService {
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person) {
+        personDao.insertPerson(person);
+        return 1;
+    }
+
+    public List<Person> getAllPeople() {
+        return personDao.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID id) {
+        return personDao.selectPersonById(id);
+    }
+
+    public int deletePerson(UUID id) {
+        return personDao.deletePersonById(id);
+    }
+
+    public int updatePerson(UUID id, Person newPerson) {
+        return personDao.updatePersonById(id, newPerson);
+    }
+}
